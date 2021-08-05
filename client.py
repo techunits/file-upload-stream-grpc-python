6c61170eb35d8afe522188f68fa3bd32b6dc82db
Sougata P <Sougata P>
2021-08-05 15:58:51 +0530

    updated client upload stream

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 # import required libraries & proto defn.
 import grpc
+import os
 from proto import uploader_pb2_grpc, uploader_pb2
 
 # initialize channel to gRPC server
@@ -9,13 +10,21 @@ channel = grpc.insecure_channel(target="localhost:50051")
 stub = uploader_pb2_grpc.UploaderServiceStub(channel=channel)
 
 # define file upload request object generator which will yield file into chunks
-def entry_request_iterator():
-    for idx in range(1, 16):
-        entry_request = uploader_pb2.FileUploadRequest(name=f"Test {idx}", 
-                                                        code=f"T{idx}",
-                                                        description=f"Test {idx} description")
-        yield entry_request
+def file_request_iterator(file_path, chunk_size=64):
+    # yield filename
+    yield uploader_pb2.FileUploadRequest(name=os.path.basename(file_path))
+    
+    # yield file data
+    with open(file_path, 'rb') as fp:
+        while True:
+            data = fp.read(chunk_size)
+            if not data:
+                break
+            yield uploader_pb2.FileUploadRequest(chunk=data)
 
-# iterate through response stream and print to console
-for entry_response in stub.FileUploadRequest(entry_request_iterator()):
-    print(entry_response)
+# stream file upload request and fetch response async
+file_path = './resources/sconnector-intro.mp4'
+upload_response_future = stub.uploadFile.future(file_request_iterator(file_path=file_path))
+upload_response = upload_response_future.result()
+
+print(upload_response)
